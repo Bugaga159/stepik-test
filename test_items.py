dfbe4275d299915db35220f2f79368d1fd0b763a
Andrew <santechserp@gmail.com>
2019-11-11 00:01:30 +0300

    update name function test

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-def test_items(browser):
+def test_guest_should_see_button_add_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     #Если нужно проверить визуально, снять коммит с time
