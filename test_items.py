575255b2a33b29d6404f90ae6a0ab726a570f915
Andrew <santechserp@gmail.com>
2019-11-10 23:42:11 +0300

    add file for lesson 3.6.9

---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+def test_items(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    #Если нужно проверить визуально, снять коммит с time
+    time.sleep(30)
+    try:
+        btn_add = browser.find_element_by_xpath('//*[@id="add_to_basket_form"]/button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
+        assert btn_add
+    except NoSuchElementException:
+        print('Button not found!')
+        assert False
